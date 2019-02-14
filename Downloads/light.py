2e713f46fcd49b1ee6e4db56bcbd8d943ad94fd4
andy9922silv <mike9922dragon@gmail.com>
2019-02-14 10:01:17 -0800

    Update light.py

--- Downloads/light.py
+++ Downloads/light.py
@@ -2,6 +2,12 @@ from datetime import datetime
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 
+#using python the hour and minute in real life is extracted
+#the LED turns on after it hits the correct time
+#will make it turn on for 8-10 hours and turn it off the rest of the time
+
+
+
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
