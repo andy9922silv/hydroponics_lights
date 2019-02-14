2649d808b53e0c0dc2ebedb8b89d0e1ae2a9b363
andy9922silv <mike9922dragon@gmail.com>
2019-02-14 09:59:11 -0800

    Update blinking_led.py

--- Downloads/blinking_led.py
+++ Downloads/blinking_led.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 
+#turns on LED and turns them off after a certain time
+#runs on the raspberry pi
+
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
