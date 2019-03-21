e8db0fb24a5742092d3642e394db1e3552ed01ad
andy9922silv <mike9922dragon@gmail.com>
2019-03-21 09:49:09 -0700

    added code to control the lights

---
+++ light_water.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep # Import the sleep function from the time module
+
+current_time = datetime.now()
+
+while True:
+    GPIO.output(8, GPIO.HIGH) # Turn on
+    sleep(600) # Sleep for 10 minutes
+    GPIO.output(8, GPIO.LOW) # Turn off
+    sleep(3600) # Sleep for 1 hour
