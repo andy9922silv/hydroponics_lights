9b7a45a76d51f2fe33ff803212d13d0e7885213e
andy9922silv <mike9922dragon@gmail.com>
2019-03-25 09:08:19 -0700

    Update light_water.py

--- light_water.py
+++ light_water.py
@@ -5,7 +5,7 @@ from time import sleep # Import the sleep function from the time module
 current_time = datetime.now()
 
 while True:
-        if(current_time.minute =0)
+        if(current_time.minute =10)
         GPIO.output(8, GPIO.HIGH) # Turn on
         sleep(600) # Sleep for 10 minutes
         GPIO.output(8, GPIO.LOW) # Turn off
