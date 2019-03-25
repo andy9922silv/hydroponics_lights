31a66b6a0e13c58a8978f9f14c7d94fb3f8e640a
andy9922silv <mike9922dragon@gmail.com>
2019-03-25 09:12:21 -0700

    Update light_water.py

--- light_water.py
+++ light_water.py
@@ -3,10 +3,11 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 
 current_time = datetime.now()
+print datetime.now()
 
-while True:
-        if(current_time.minute == 10)
-        GPIO.output(8, GPIO.HIGH) # Turn on
-        sleep(600) # Sleep for 10 minutes
-        GPIO.output(8, GPIO.LOW) # Turn off
-        sleep(3600) # Sleep for 1 hour
+#while True:
+        #if(current_time.minute =0)
+        #GPIO.output(8, GPIO.HIGH) # Turn on
+        #sleep(600) # Sleep for 10 minutes
+        #GPIO.output(8, GPIO.LOW) # Turn off
+        #sleep(3600) # Sleep for 1 hour
