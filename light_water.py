423d2fc94d56271fa548ba4981a6c20e13067bf4
andy9922silv <mike9922dragon@gmail.com>
2019-03-25 09:21:45 -0700

    Update light_water.py

--- light_water.py
+++ light_water.py
@@ -2,13 +2,18 @@ from datetime import datetime
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+
 current_time = datetime.now()
 print datetime.now()
 print current_time.minute
 print (current_time.hour-7)
 
 while True:
-        if(current_time.minute == 21):
+        if(current_time.minute == 22):
                 GPIO.output(8, GPIO.HIGH) # Turn on
                 sleep(600) # Sleep for 10 minutes
                 GPIO.output(8, GPIO.LOW) # Turn off
