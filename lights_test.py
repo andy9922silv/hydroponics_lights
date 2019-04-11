479534fa39c699efe1e687653440b9016efab0dd
andy9922silv <mike9922dragon@gmail.com>
2019-04-11 09:37:58 -0700

    Update lights_test.py

--- lights_test.py
+++ lights_test.py
@@ -5,7 +5,7 @@ from time import sleep # Import the sleep function from the time module
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 GPIO.setmode(GPIO.BOARD)
 
 current_time = datetime.now()
@@ -18,7 +18,7 @@ while True:
     minute = datetime.now().minute
     hour = datetime.now().hour
     if (minute % 2 == 0):
-        GPIO.output(8, GPIO.HIGH) # Turn on
+        GPIO.output(19, GPIO.HIGH) # Turn on
     if (minute % 2 == 1):
-        GPIO.output(8, GPIO.LOW) # Turn off
+        GPIO.output(19, GPIO.LOW) # Turn off
 
