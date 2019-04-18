6f85411b888edcb5781040d5624e80da51273172
andy9922silv <mike9922dragon@gmail.com>
2019-04-18 09:57:52 -0700

    Update pump_test.py

--- pump_test.py
+++ pump_test.py
@@ -6,6 +6,7 @@ from time import sleep # Import the sleep function from the time module
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 GPIO.setmode(GPIO.BOARD)
 
 current_time = datetime.now()
@@ -19,5 +20,7 @@ while True:
     hour = (datetime.now().hour-8)
     if (minute % 2 == 0):
         GPIO.output(3, GPIO.HIGH) # Turn on
+        GPIO.output(19, GPIO.HIGH) # Turn on
     if (minute % 2 == 1):
         GPIO.output(3, GPIO.LOW) # Turn off
+        GPIO.output(19, GPIO.LOW) # Turn off
